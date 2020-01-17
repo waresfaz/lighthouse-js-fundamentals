3845c30bf1e7473c5fe84464bb411d2366a91a46
Wares <fazelyar.wares@gmail.com>
2020-01-17 01:57:59 +0000

    using for loops and while loops with arrays

--- packing-list.js
+++ packing-list.js
@@ -5,3 +5,10 @@ for
 (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
